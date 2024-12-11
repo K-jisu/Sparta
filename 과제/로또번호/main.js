6b561776876738bf984644bcf73f46b6a744f508
KJSoo95 <wltn7star@naver.com>
2024-12-11 16:44:44 +0900

    로또번호 수정

--- 과제/로또번호/main.js
+++ 과제/로또번호/main.js
@@ -1,21 +1,40 @@
 window.onload = () => {
   const genBtn = document.getElementById("generateButton");
   const output = document.getElementById("numbers");
+  const prenum = document.getElementById("prenum");
 
+  let prevnumbers = [];
   // ********************* genBtn*************
   genBtn.addEventListener("click", function () {
     let numbers = generateLottoNumbers();
-    console.log(numbers);
+    // console.log(numbers);
     output.textContent = numbers.join(", ");
+    // console.log(prevnumbers)
+    prenum.textContent = prevnumbers.join(", ");
+    prevnumbers = [...numbers];
+    // console.log(prevnumbers)
   });
 
   // **********************generateLottoNumbers() *********************
   function generateLottoNumbers() {
     let arrContainer = [];
-    for (i = 0; i < 6; i++) {
+    output.classList.remove("animation");
+    prenum.classList.remove("animation");
+    // for (i = 0; i < 6; i++) {
+    //   const ranNum = Math.floor(Math.random() * 45) + 1;
+
+    //   arrContainer.includes(ranNum) || arrContainer.push(ranNum);
+    // }
+    while (arrContainer.length < 6) {
       const ranNum = Math.floor(Math.random() * 45) + 1;
+
       arrContainer.includes(ranNum) || arrContainer.push(ranNum);
     }
-    return arrContainer;
+
+    setTimeout(() => {
+      output.classList.add("animation");
+      prenum.classList.add("animation");
+    }, 10);
+    return arrContainer.sort((a, b) => a - b);
   }
 };
